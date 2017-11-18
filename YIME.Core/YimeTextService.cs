c0cd956437b165fb83f162f96e3fdbf762b74f16
Copy Liu <copyliu@gmail.com>
2017-11-18 21:34:18 +0800

    some stuff

---
+++ YIME.Core/YimeTextService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TSF.InteropTypes;
+using TSF.TypeLib;
+
+namespace YIME.Core
+{
+    [ComVisible(true)]
+    [Guid("1B4A4B30-B810-4483-8CE6-A5FF82835999")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class YimeTextService : ITfTextInputProcessorEx, ITfThreadMgrEventSink, ITfThreadFocusSink, ITfDisplayAttributeProvider
+    {
+        HRESULT ITfTextInputProcessor.Activate(ITfThreadMgr ptim, uint tid)
+        {
+            throw new NotImplementedException();
+        }
+
+        HRESULT ITfTextInputProcessorEx.Deactivate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT ActivateEx(ITfThreadMgr ptim, uint tid, TF_TMAE dwFlags)
+        {
+            throw new NotImplementedException();
+        }
+
+        HRESULT ITfTextInputProcessorEx.Activate(ITfThreadMgr ptim, uint tid)
+        {
+            throw new NotImplementedException();
+        }
+
+        HRESULT ITfTextInputProcessor.Deactivate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT OnInitDocumentMgr(ITfDocumentMgr pdim)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT OnUninitDocumentMgr(ITfDocumentMgr pdim)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT OnSetFocus(ITfDocumentMgr pdimFocus, ITfDocumentMgr pdimPrevFocus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT OnPushContext(ITfContext pic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT OnPopContext(ITfContext pic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT OnSetThreadFocus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT OnKillThreadFocus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT EnumDisplayAttributeInfo(out IEnumTfDisplayAttributeInfo ppEnum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HRESULT GetDisplayAttributeInfo(ref Guid guid, out ITfDisplayAttributeInfo ppInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- YIME/Program.cs
+++ YIME/Program.cs
@@ -1,15 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using TSF.InteropTypes;
+using TSF.TypeLib;
 
 namespace YIME
 {
     class Program
     {
+        private static Guid CLSID_TEXTSERVICE = new Guid("1B4A4B30-B810-4483-8CE6-A5FF82835999");
+        private static Guid GUID_PROFILE = new Guid("661B35E7-4637-47F3-8E13-6CEA1E9C6E7E");
+        private static string name = "猫咪输入法";
+        static Guid[] SupportCategories = new[]{
+            TSF.TypeLib.Guids.GUID_TFCAT_TIP_KEYBOARD,
+            TSF.TypeLib.Guids.GUID_TFCAT_DISPLAYATTRIBUTEPROVIDER,
+            TSF.TypeLib.Guids.GUID_TFCAT_TIPCAP_UIELEMENTENABLED,
+            TSF.TypeLib.Guids.GUID_TFCAT_TIPCAP_SECUREMODE,
+            TSF.TypeLib.Guids.GUID_TFCAT_TIPCAP_COMLESS,
+            TSF.TypeLib.Guids.GUID_TFCAT_TIPCAP_INPUTMODECOMPARTMENT,
+            TSF.TypeLib.Guids.GUID_TFCAT_TIPCAP_IMMERSIVESUPPORT, // WIN8 ONLY
+            TSF.TypeLib.Guids.GUID_TFCAT_TIPCAP_SYSTRAYSUPPORT, // WIN8 ONLY
+        };
+        [STAThread]
         static void Main(string[] args)
         {
+            if (args.Length > 0 && args[0].ToLower() == "uninstall")
+            {
+                Unregister();
+            }
+            else
+            {
+                Register();
+            }
+            
         }
+        [STAThread]
+        public static void Unregister()
+        {
+
+            ITfCategoryMgr c = new ITfCategoryMgr();
+            foreach (var supportCategory in SupportCategories)
+            {
+                var r = c.UnregisterCategory(CLSID_TEXTSERVICE, supportCategory, CLSID_TEXTSERVICE);
+                Console.WriteLine(r.ToString());
+            }
+            ITfInputProcessorProfiles p = new ITfInputProcessorProfiles();
+            var result = p.Unregister(CLSID_TEXTSERVICE);
+
+            Console.WriteLine(result.ToString());
+            var regservice = new RegistrationServices();
+            regservice.UnregisterAssembly(Assembly.LoadFile("YIME.Core.dll"));
+        }
+        [STAThread]
+        public static void Register()
+        {
+            var regservice = new RegistrationServices();
+            regservice.RegisterAssembly(Assembly.LoadFile("YIME.Core.dll"),
+                AssemblyRegistrationFlags.SetCodeBase);
+            var iconname = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location),
+                "YIME.Core.dll");
+            Console.WriteLine(iconname);
+            Console.WriteLine(CultureInfo.GetCultureInfo("zh-cn").LCID);
+            //Insert code here.
+            var p = new ITfInputProcessorProfiles();
+            var result = p.Register(CLSID_TEXTSERVICE);
+
+            Console.WriteLine(result.ToString());
+            result = p.AddLanguageProfile(CLSID_TEXTSERVICE, new LangID((ushort)CultureInfo.GetCultureInfo("zh-cn").LCID), GUID_PROFILE,
+                name.ToCharArray(), (uint)name.ToCharArray().Length, iconname.ToCharArray(), (uint)iconname.Length, 0);
+
+            Console.WriteLine(result.ToString());
+            var c = new ITfCategoryMgr();
+            foreach (var supportCategory in SupportCategories)
+            {
+                var r = c.RegisterCategory(CLSID_TEXTSERVICE, supportCategory, CLSID_TEXTSERVICE);
+                Console.WriteLine(r.ToString());
+            }
+
+        }
+
     }
 }
