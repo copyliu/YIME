dd82846aa069d4f3953b67f5920d81a5ce794fd6
Copy Liu <copyliu@gmail.com>
2021-12-18 18:14:32 +0800

    remove RIME

--- build_librime.bat
+++
@@ -1,8 +0,0 @@
-cd librime
-call env.bat
-set PATH=%PATH%;%CMAKE_INSTALL_PATH%\bin
-call "%VS_INSTALL_PATH%\VC\vcvarsall.bat" x86
-call build.bat boost
-call build.bat thirdparty
-call build.bat librime
-copy build\lib\Release\rime.dll ..\YIME\rime.dll
